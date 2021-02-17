bf45ca95f79f953287b6deef6001559628c2b472
Eugene Akulov <eugenakv@gmail.com>
2021-02-17 18:10:53 +0300

    Let there be light

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+namespace UsersSheet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId
+        {
+            get; set;
+        }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
